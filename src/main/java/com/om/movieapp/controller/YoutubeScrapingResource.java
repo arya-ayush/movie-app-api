fafd6492a2cfb31ff3b67ab908833e8ed8c5e560
Ayush Arya <ayush@kwench.in>
2020-04-13 01:37:10 +0530

    [fetaure] youtube search basic complte

--- src/main/java/com/om/movieapp/controller/YoutubeScrapingResource.java
+++ src/main/java/com/om/movieapp/controller/YoutubeScrapingResource.java
@@ -1,22 +1,30 @@
 package com.om.movieapp.controller;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.jetty.http.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.om.movieapp.exception.ApplicationException;
 import com.om.movieapp.model.Youtube;
 import com.om.movieapp.service.YoutubeScrapingService;
+import com.om.movieapp.utils.messages.Messages;
 
 @Path("youtube")
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class YoutubeScrapingResource {
 
   private static final Logger LOG = LoggerFactory.getLogger(YoutubeScrapingResource.class);
@@ -25,8 +33,14 @@ public class YoutubeScrapingResource {
   private YoutubeScrapingService youtubeScrapingService;
 
   @GET
-  public Response fetchData() {
-    List<Youtube> youtubeList = youtubeScrapingService.fetchResults("Latest hollywood full movie");
+  @Path("search")
+  public Response fetchData(@QueryParam("query") String query) {
+    LOG.info("fetchData - Method begins here with query <{}>", query);
+    if (StringUtils.isEmpty(query)) {
+      throw new ApplicationException(HttpStatus.BAD_REQUEST_400,
+          new String(Messages.INCORRECT_PARAMETERS.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8));
+    }
+    List<Youtube> youtubeList = youtubeScrapingService.fetchResults(query);
     return Response.ok(youtubeList).build();
   }
 }
---
+++ src/main/java/com/om/movieapp/utils/messages/Messages.java
@@ -0,0 +1,11 @@
+package com.om.movieapp.utils.messages;
+
+public final class Messages {
+
+  public static final String INCORRECT_PARAMETERS = "Request contains invalid parameters";
+
+  private Messages() {
+
+  }
+
+}
