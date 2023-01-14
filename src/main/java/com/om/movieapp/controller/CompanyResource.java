354273fdef9aaea759cd8335111a609eec151cf7
Ayush Arya <arya.ayush15@gmail.com>
2023-01-14 15:12:58 +0530

    version update 4.3.3(56)

--- src/main/java/com/om/movieapp/controller/CompanyResource.java
+++ src/main/java/com/om/movieapp/controller/CompanyResource.java
@@ -35,9 +35,9 @@ public class CompanyResource {
     uChatApp.setName("Status Saver - All in One");
 //    otherApps.add(statusSaverApp);
     App app = new App();
-    app.setVersionCode(50);
+    app.setVersionCode(56);
     app.setPackageName("com.om.fullmovie");
-    app.setVersion("4.2.0");
+    app.setVersion("4.3.3");
     Company company = new Company();
     company.setName("Omtech");
     company.setOtherApps(otherApps);
