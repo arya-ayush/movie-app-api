1c8fed26db116bfa79551b8bdbdf6bf4ae8bfcc4
divyanshu <techistics1@gmail.com>
2025-04-10 01:36:41 +0530

    CI/CD Test

--- src/main/java/com/om/movieapp/service/MovieService.java
+++ src/main/java/com/om/movieapp/service/MovieService.java
@@ -129,7 +129,7 @@ public class MovieService {
 
 
         MovieDetail movie = new MovieDetail();
-        movie.setName(movieDetail.getTitle());
+        movie.setName("Dwwfwe");
         movie.setDescription(movieDetail.getPlot());
         movie.setMovieUrl(mp4Url);
         movie.setRuntime(movieDetail.getRuntime());
